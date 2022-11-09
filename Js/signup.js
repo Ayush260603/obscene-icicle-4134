382c5d2a0a4d0ff3802b8d66a590f6de19673370
KunalMehra075 <112753481+KunalMehra075@users.noreply.github.com>
2022-11-10 01:11:06 +0530

    Day1TasksDone

---
+++ Js/signup.js
@@ -0,0 +1,9 @@
+document.querySelector("#registerbut").addEventListener("click",function(){
+    window.location.href="signup.html"
+});
+var loader = document.querySelector("#preloader");
+window.addEventListener("load",function(){
+    loader.style.display="none";
+})
+
+
